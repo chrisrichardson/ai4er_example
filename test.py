6a2a6fdfdaae9d3461ec8bf2ac0bc0623bb6bda0
Chris Richardson <chris@bpi.cam.ac.uk>
2022-09-28 14:51:41 +0100

    Add more tests

--- test.py
+++ test.py
@@ -1,6 +1,14 @@
 
 from summation import summation
 
-def test_example():
 
+def test_example():
     assert summation(10) == 45
+
+
+def test_example2():
+    assert summation(11) == 55
+
+
+def test_example3():
+    assert summation(-1) == 0
