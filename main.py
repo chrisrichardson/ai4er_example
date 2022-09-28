d84992249ad414d12f25e3b5c6f4f0a2a11f11c9
Chris Richardson <chris@bpi.cam.ac.uk>
2022-09-28 14:37:28 +0100

    Fix style errors

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-x = np.linspace(0,4*np.pi,100)
+x = np.linspace(0, 4*np.pi, 100)
 y = np.sin(x)
 y2 = np.cos(x)
 y3 = y/y2
@@ -10,6 +10,6 @@ y3 = y/y2
 plt.plot(x, y)
 plt.plot(x, y2)
 plt.plot(x, y3)
-plt.ylim((-4,4))
+plt.ylim((-4, 4))
 
 plt.show()
